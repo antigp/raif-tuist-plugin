fabafb2e1f6010a59c0f21bdf7c3903581c7b292
Evgeny Antropov <Evgeny.Antropov@raiffeisen.ru>
2022-05-19 19:50:19 +0300

    Init commit

---
+++ Sources/CLI/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by ANTROPOV Evgeny on 19.05.2022.
+//
+
+import Foundation
---
+++ Sources/RunShell/RunShell.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by ANTROPOV Evgeny on 19.05.2022.
+//
+
+import Foundation
+
+func shell(_ command: String) throws {
+    enum Terminate: Error {
+        case status(Int32)
+    }
+    let task = Process()
+    
+    let fileManager = FileManager.default
+    var sourceRC = ""
+    let homeDirURL = URL(fileURLWithPath: NSHomeDirectory()).path
+    if fileManager.fileExists(atPath: "\(homeDirURL)/.bashrc") {
+        sourceRC += "source \(homeDirURL)/.bashrc &&"
+    }
+    if fileManager.fileExists(atPath: "\(homeDirURL)/.zshrc") {
+        sourceRC += "source \(homeDirURL)/.zshrc &&"
+    }
+    task.standardOutput = FileHandle.standardOutput
+    task.standardError = FileHandle.standardError
+    task.arguments = ["--login", "-c", "export HOME=\(homeDirURL) && export LANG=en_US.UTF-8 && \(sourceRC)" + command]
+    task.launchPath = "/bin/bash"
+    task.standardInput = nil
+    task.environment = ["HOME": homeDirURL]
+    task.currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+    task.launch()
+    
+    task.waitUntilExit()
+    
+    guard task.terminationStatus == 0 else {
+        throw Terminate.status(task.terminationStatus)
+    }
+}
