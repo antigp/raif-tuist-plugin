d1043ba8888045b60acc78105cfa1e0be39c164b
Evgeny Antropov <Evgeny.Antropov@raiffeisen.ru>
2022-05-26 09:23:07 +0300

    Version 0.20

--- Sources/CI/main.swift
+++ Sources/CI/main.swift
@@ -19,6 +19,7 @@ do {
         try shell("bundle install")
     }
     let homeDirURL = URL(fileURLWithPath: NSHomeDirectory())
+    
     if (
         (try? shell("bundle exec pod repo-art list | grep cocoapods-art")) == nil ||
         !FileManager.default.fileExists(atPath: "\(homeDirURL.path)/.cocoapods/repos-art/cocoapods-art/.artpodrc")
@@ -26,7 +27,14 @@ do {
         try? shell("bundle exec pod repo-art remove cocoapods-art")
         try shell("bundle exec pod repo-art add cocoapods-art \"https://artifactory.raiffeisen.ru/artifactory/api/pods/cocoapods\"")
     } else {
-        try shell("bundle exec pod repo-art update cocoapods-art")
+        let artifactoryUpdateTime = fileModificationDate(path: "\(homeDirURL.path)/.cocoapods/repos-art/cocoapods-art/.artpodrc")
+        if let artifactoryUpdateTime = artifactoryUpdateTime,
+           Date().timeIntervalSince(artifactoryUpdateTime) > 24 * 60 * 60 {
+            print("Artifactory needs to be updates: \(artifactoryUpdateTime)")
+            try shell("bundle exec pod repo-art update cocoapods-art")
+        } else {
+            print("Artifactory already updated: \(artifactoryUpdateTime)")
+        }
     }
         
     try shell("./scripts/generator -e _Prebuild")
@@ -37,3 +45,11 @@ do {
 }
 
 
+func fileModificationDate(path: String) -> Date? {
+    do {
+        let attr = try FileManager.default.attributesOfItem(atPath: path)
+        return attr[FileAttributeKey.modificationDate] as? Date
+    } catch {
+        return nil
+    }
+}
